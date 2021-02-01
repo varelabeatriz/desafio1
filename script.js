19be563b51e7897ce9540b2c70a81406c710f6fd
Gabriel Souza Pereira <gabrielspereira2604@gmail.com>
2021-01-31 21:43:25 -0300

    Add Jquery Functions

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const sizes = document.querySelector(".sizes div");
-sizes.onclick = function(){}
+function trocaImagem (id) {
+    if(id == 1){
+        $('#imagemDinamica').attr('src', './img/roxo.png');
+    }else if(id == 2){
+        $('#imagemDinamica').attr('src', './img/verde.png');
+    }else{
+        $('#imagemDinamica').attr('src', './img/azul.png');
+    }
+};
 
-function selectSize(){
-    document.style.background = white;
-}
+function trocaTamanho (tamanho) {
+    $('.sizes div').removeClass('active');
+    $('#tamanho' + tamanho).addClass('active');
+};
