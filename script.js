9c578d2dcbf812cc572be220698fd402baf88c4e
Beatriz Varela <beatrizsv19@gmail.com>
2021-01-31 22:10:53 -0300

    color styles

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function trocaImagem (id) {
     if(id == 1){
         $('#imagemDinamica').attr('src', './img/roxo.png');
+        $('.row div').removeClass('active');
+        $('.purple').addClass('active');
     }else if(id == 2){
         $('#imagemDinamica').attr('src', './img/verde.png');
+        $('.row div').removeClass('active');
+        $('.row .green').addClass('active');
     }else{
         $('#imagemDinamica').attr('src', './img/azul.png');
+        $('.row div').removeClass('active');
+        $('.blue').addClass('active');
     }
 };
 
